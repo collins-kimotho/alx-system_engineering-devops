d9ac900674f2fc0765b62ba945e7c7e5812dc326
collins-kimotho <collinskimotho16@gmail.com>
2024-07-30 03:19:03 -0700

    install-flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-# Install flask
-package { 'flask':
+# This Puppet manifest installs Flask version 2.1.0 using pip3
+
+package { 'Flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
 }
